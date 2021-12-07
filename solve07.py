8546ff6959b66bb3355c4beb60cbba6059acb94c
Tobias <tobias_maier99@gmx.de>
2021-12-07 18:23:38 +0100

    day7

--- solve07.py
+++ solve07.py
@@ -3,24 +3,41 @@ from aocd import get_data, submit
 
 
 def main():
-    lines = get_data(day=7, year=2021)
-    testlines = [""]
-    assert solve07(testlines) == 0
+    lines = [int(x) for x in get_data(day=7, year=2021).split(",")]
+    testlines = [int(x) for x in "16,1,2,0,4,2,7,1,2,14".split(",")]
+    testresult = solve07(testlines)
+    assert testresult == 37, f"{testresult} != 37"
     result = solve07(lines)
     print("solve07:", result)
     submit(result, part="a", day=7, year=2021)
-    assert solve07_2(testlines) == 0
+    testresult2 = solve07_2(testlines)
+    assert testresult2 == 168, f"{testresult2} != 168"
     result2 = solve07_2(lines)
     print("solve07_2:", result2)
     submit(result2, part="b", day=7, year=2021)
 
 
-def solve07(lines):
-    ...
+def solve07(positions):
+    result = 0
+    for target in range(max(positions)):
+        res = 0
+        for position in positions:
+            res += abs(position - target)
+        if result == 0 or res <= result:
+            result = res
+    return result
 
 
-def solve07_2(lines):
-    ...
+def solve07_2(positions):
+    result = 0
+    for target in range(max(positions)):
+        res = 0
+        for position in positions:
+            n = abs(position - target)
+            res += ((n * (n + 1)) // 2)
+        if result == 0 or res <= result:
+            result = res
+    return result
 
 
 if __name__ == "__main__":
