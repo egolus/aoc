09169523505c9c4b4573e0dda8cec8a68a1365fa
Tobias <tobias_maier99@gmx.de>
2021-12-09 11:11:10 +0100

    day 09

--- solve09.py
+++ solve09.py
@@ -3,10 +3,14 @@ from aocd import get_data, submit
 
 
 def main():
-    values = [int(x) for x in get_data(day=9, year=2021).split(",")]
-    testvalues = [int(x) for x in "16,1,2,0,4,2,7,1,2,14".split(",")]
-    testtrue = 0
-    testtrue2 = 0
+    values = get_data(day=9, year=2021).split("\n")
+    testvalues = """2199943210
+3987894921
+9856789892
+8767896789
+9899965678""".split("\n")
+    testtrue = 15
+    testtrue2 = 1134
 
     testresult = solve09(testvalues)
     assert testresult == testtrue, f"{testresult} != {testtrue}"
@@ -26,11 +30,95 @@ def main():
 
 
 def solve09(values):
-    ...
+    """
+    risk-level(low-point) = 1 + height
+    -> 2, 1, 6, 6
+
+    sum(risk-levels) = 2 + 1 + 6 + 6 = 15
+    """
+    print("values:", "\n".join(values), sep="\n")
+    result = 0
+    height = len(values)
+    for i in range(height):
+        width = len(values[0])
+        for j in range(width):
+            v = values[i][j]
+            others = []
+            if i == 0:
+                others += [values[i + 1][j]]
+            elif i == height - 1:
+                others += [values[i - 1][j]]
+            else:
+                others += [values[i - 1][j], values[i + 1][j]]
+            if j == 0:
+                others += [values[i][j + 1]]
+            elif j == width - 1:
+                others += [values[i][j - 1]]
+            else:
+                others += [values[i][j - 1], values[i][j + 1]]
+            if v < min(others):
+                result += 1 + int(v)
+    return result
 
 
 def solve09_2(values):
-    ...
+    result = 1
+    height = len(values)
+    width = len(values[0])
+    basins = []
+    checkedValues = set()
+
+    for i in range(height):
+        for j in range(width):
+            v = values[i][j]
+            others = []
+            if i == 0:
+                others += [values[i + 1][j]]
+            elif i == height - 1:
+                others += [values[i - 1][j]]
+            else:
+                others += [values[i - 1][j], values[i + 1][j]]
+            if j == 0:
+                others += [values[i][j + 1]]
+            elif j == width - 1:
+                others += [values[i][j - 1]]
+            else:
+                others += [values[i][j - 1], values[i][j + 1]]
+            if v < min(others):
+                basins.append([(i, j)])
+                checkedValues.add((i, j))
+    for basin in basins:
+        for point in basin:
+            if point[0] > 0:
+                other = (point[0] - 1, point[1])
+                if other not in checkedValues:
+                    if values[other[0]][other[1]] < "9":
+                        basin.append(other)
+                        checkedValues.add(other)
+            if point[0] < height - 1:
+                other = (point[0] + 1, point[1])
+                if other not in checkedValues:
+                    if values[other[0]][other[1]] < "9":
+                        basin.append(other)
+                        checkedValues.add(other)
+            if point[1] > 0:
+                other = (point[0], point[1] - 1)
+                if other not in checkedValues:
+                    if values[other[0]][other[1]] < "9":
+                        basin.append(other)
+                        checkedValues.add(other)
+            if point[1] < width - 1:
+                other = (point[0], point[1] + 1)
+                if other not in checkedValues:
+                    if values[other[0]][other[1]] < "9":
+                        basin.append(other)
+                        checkedValues.add(other)
+
+    basinlengths = sorted(len(basin) for basin in basins)
+    print(f"basinlengths: {basinlengths}")
+    for i in basinlengths[-3:]:
+        result = result * i
+    return result
 
 
 if __name__ == "__main__":
